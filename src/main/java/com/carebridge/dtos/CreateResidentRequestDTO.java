879eb70815d5c769c49962c0e06dd1bc0a9541d0
MojoOno <Ffranck0406@gmail.com>
2025-11-28 13:49:28 +0100

    added some id's to the CRDTO

--- src/main/java/com/carebridge/dtos/CreateResidentRequestDTO.java
+++ src/main/java/com/carebridge/dtos/CreateResidentRequestDTO.java
@@ -4,13 +4,17 @@ public class CreateResidentRequestDTO {
     private String firstName;
     private String lastName;
     private String cprNr;
+    private String userId;
+    private String guardianId;
 
     public CreateResidentRequestDTO() {}
 
-    public CreateResidentRequestDTO(String firstName, String lastName, String cprNr) {
+    public CreateResidentRequestDTO(String firstName, String lastName, String cprNr, String userId, String guardianId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.cprNr = cprNr;
+        this.userId = userId;
+        this.guardianId = guardianId;
     }
 
     public String getFirstName() { return firstName; }
@@ -21,4 +25,10 @@ public class CreateResidentRequestDTO {
 
     public String getCprNr() { return cprNr; }
     public void setCprNr(String cprNr) { this.cprNr = cprNr; }
+
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+
+    public String getGuardianId() { return guardianId; }
+    public void setGuardianId(String guardianId) { this.guardianId = guardianId; }
 }
