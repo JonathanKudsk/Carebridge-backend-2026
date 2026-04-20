0c70939b98097faa152e7beded8630008aa9e468
Abbas Badreddine <abbas.bdr@hotmail.com>
2026-04-20 22:57:07 +0200

    Editted Shift entity

--- src/main/java/com/carebridge/entities/Shift.java
+++ src/main/java/com/carebridge/entities/Shift.java
@@ -1,11 +1,6 @@
 package com.carebridge.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
 @Entity
-@Table(name = "shift")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -26,30 +19,21 @@ public class Shift {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "start_shift", nullable = false)
 	private LocalDateTime startShift;
 
-	@Column(name = "end_shift", nullable = false)
 	private LocalDateTime endShift;
 
-	@Column(name = "shift_type", nullable = false)
 	private String shiftType;
 
-	@Column(name = "location", nullable = false)
 	private String location;
 
-	@Column(name = "status", nullable = false)
 	private String status;
 
-	@Column(name = "plan_period_id", nullable = false)
 	private Long planPeriodId;
 
-	@Column(name = "assigned_user_id")
 	private Long assignedUserId;
 
-	@Column(name = "created_by", nullable = false)
 	private Long createdBy;
 
-	@Column(name = "created_at", nullable = false)
 	private LocalDateTime createdAt = LocalDateTime.now();
 }
