c8c0f2349e9ccb66877aa6ce2b12c30e6c5a0835
Benjamin Rino <benjar.rino@gmail.com>
2026-04-21 14:47:35 +0200

    Pasted task 4 into this (task 6), to make the test succeed and pass the Suit lint test

---
+++ src/main/java/com/carebridge/dtos/TotpCodeRequest.java
@@ -0,0 +1,15 @@
+package com.carebridge.dtos;
+
+public class TotpCodeRequest {
+    private String code;
+
+    public TotpCodeRequest() {}
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
