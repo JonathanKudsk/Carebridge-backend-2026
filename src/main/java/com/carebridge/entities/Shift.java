baa139fd991e2d84b2f280e878a5693f80903033
Abbas M. Badreddine <abbas.bdr@hotmail.com>
2026-04-21 09:28:46 +0200

    Editted Shift entity, så JPA is implemented.

--- src/main/java/com/carebridge/entities/Shift.java
+++ src/main/java/com/carebridge/entities/Shift.java
@@ -19,21 +19,30 @@ public class Shift {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "start_shift", nullable = false)
 	private LocalDateTime startShift;
 
+	@Column(name = "end_shift", nullable = false)
 	private LocalDateTime endShift;
 
+	@Column(name = "shift_type", nullable = false)
 	private String shiftType;
 
+	@Column(name = "location", nullable = false)
 	private String location;
 
+	@Column(name = "status", nullable = false)
 	private String status;
 
+	@Column(name = "plan_period_id", nullable = false)
 	private Long planPeriodId;
 
+	@Column(name = "assigned_user_id", nullable = false)
 	private Long assignedUserId;
 
+	@Column(name = "created_by", nullable = false)
 	private Long createdBy;
 
+	@Column(name = "created_at", nullable = false)
 	private LocalDateTime createdAt = LocalDateTime.now();
 }
