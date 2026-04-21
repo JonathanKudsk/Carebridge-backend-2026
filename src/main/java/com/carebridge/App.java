75f135d004c071a4041a30622e9fb3a806d5a3e9
MariamE04 <elmirmariam16@gmail.com>
2026-04-21 10:31:08 +0200

    fået backenden til at køre

--- src/main/java/com/carebridge/App.java
+++ src/main/java/com/carebridge/App.java
@@ -1,6 +1,9 @@
 package com.carebridge;
 
 import com.carebridge.config.ApplicationConfig;
+import com.carebridge.dao.impl.UserDAO;
+import com.carebridge.entities.User;
+import com.carebridge.entities.enums.Role;
 import io.javalin.Javalin;
 
 public class App {
@@ -9,5 +12,10 @@ public class App {
         Javalin app = ApplicationConfig.startServer(7070);
 
         app.get("/", ctx -> ctx.result("Carebridge API is running"));
+
+        User user1 = new User("admin", "admin@gmail.com", "1234", Role.ADMIN);
+
+        UserDAO userDAO = UserDAO.getInstance();
+        userDAO.create(user1);
     }
 }
