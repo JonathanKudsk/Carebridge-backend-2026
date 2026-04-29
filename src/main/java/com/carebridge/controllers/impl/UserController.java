0f9cc6640d83fc67ffb149e41db67482b37684ad
sofus <sofus@k7c.dk>
2026-04-29 10:19:33 +0200

    fjernet en fejl importering

--- src/main/java/com/carebridge/controllers/impl/UserController.java
+++ src/main/java/com/carebridge/controllers/impl/UserController.java
@@ -17,7 +17,6 @@ import io.javalin.http.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.text.html.parser.Parser;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
