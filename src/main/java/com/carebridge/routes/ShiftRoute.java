8b5363fdaabdb75641db7b11305cd47dcca43f14
Styrse <styrbjoern@me.com>
2026-05-14 02:16:15 +0200

    Added delete endpoint and implemented in controller

--- src/main/java/com/carebridge/routes/ShiftRoute.java
+++ src/main/java/com/carebridge/routes/ShiftRoute.java
@@ -4,6 +4,7 @@ import com.carebridge.controllers.impl.ShiftController;
 import com.carebridge.entities.enums.Role;
 import io.javalin.apibuilder.EndpointGroup;
 
+import static io.javalin.apibuilder.ApiBuilder.delete;
 import static io.javalin.apibuilder.ApiBuilder.put;
 import static io.javalin.apibuilder.ApiBuilder.post;
 
@@ -15,6 +16,7 @@ public class ShiftRoute {
         return () -> {
             post("/", shiftController::create, Role.PLANNER, Role.ADMIN);
             put("/{id}", shiftController::update, Role.PLANNER, Role.ADMIN);
+            delete("/{id}", shiftController::delete, Role.PLANNER, Role.ADMIN);
         };
     }
 }
