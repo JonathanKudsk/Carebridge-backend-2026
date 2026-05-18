61c580801b500ac5fec39ef48171888133ed6cbe
Frederik-BipBop <Bastiansen44@gmail.com>
2026-05-18 10:15:32 +0200

    created validation in shiftcontroller + added a put route for the update method

--- src/main/java/com/carebridge/routes/ShiftRoute.java
+++ src/main/java/com/carebridge/routes/ShiftRoute.java
@@ -4,7 +4,7 @@ import com.carebridge.controllers.impl.ShiftController;
 import com.carebridge.entities.enums.Role;
 import io.javalin.apibuilder.EndpointGroup;
 
-import static io.javalin.apibuilder.ApiBuilder.post;
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class ShiftRoute {
 
@@ -13,6 +13,10 @@ public class ShiftRoute {
     public EndpointGroup getRoutes() {
         return () -> {
             post("/", shiftController::create, Role.PLANNER, Role.ADMIN);
+            post(shiftController::create, Role.PLANNER, Role.ADMIN);
+            path("{id}", () -> {
+                put(shiftController::update, Role.PLANNER, Role.ADMIN);
+            });
+        };
         };
     }
-}
